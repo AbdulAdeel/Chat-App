a2f0b0f59031972cea9b3b946095a26facb3f634
sethnr3 <sethrajarathne@gmail.com>
2024-12-30 01:13:15 +0530

    completed upto 31

--- src/redux/slices/conversation.js
+++ src/redux/slices/conversation.js
@@ -1,7 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const user_id = window.localStorage.getItem("user_id");
 
 const initialState = {
@@ -28,24 +27,77 @@ const slice = createSlice({
           name: `${this_user?.firstName} ${this_user?.lastName}`,
           online: this_user?.status === "Online",
           img: faker.image.avatar(),
-          msg: faker.music.songName(), 
+          msg: faker.music.songName(),
           time: "9:36",
           unread: 0,
           pinned: false,
-          
         };
       });
-        state.direct_chat.conversations = list;
+      state.direct_chat.conversations = list;
     },
-},
+
+    updateDirectConversation(state, action) {
+      const this_conversation = action.payload.conversation;
+      state.direct_chat.conversations = state.direct_chat.conversations.map(
+        (el) => {
+          if (el?.id !== this_conversation._id) {
+            return el;
+          } else {
+            const user = this_conversation.participants.find(
+              (elm) => elm._id.toString() !== user_id
+            );
+            return {
+              id: this_conversation._id._id,
+              user_id: user?._id,
+              name: `${user?.firstName} ${user?.lastName}`,
+              online: user?.status === "Online",
+              img: faker.image.avatar(),
+              msg: faker.music.songName(),
+              time: "9:36",
+              unread: 0,
+              pinned: false,
+            };
+          }
+        }
+      );
+    },
+    addDirectConversation(state, action) {
+      const this_conversation = action.payload.conversation;
+      const user = this_conversation.participants.find(
+        (elm) => elm._id.toString() !== user_id
+      );
+      state.direct_chat.conversations = state.direct_chat.conversations.filter(
+        (el) => el?.id !== this_conversation._id
+      );
+      state.direct_chat.conversations.push({
+        id: this_conversation._id._id,
+        user_id: user?._id,
+        name: `${user?.firstName} ${user?.lastName}`,
+        online: user?.status === "Online",
+        img: faker.image.avatar(),
+        msg: faker.music.songName(),
+        time: "9:36",
+        unread: 0,
+        pinned: false,
+      });
+    },
+  },
 });
 
-
 export default slice.reducer;
-
 
 export const FetchDirectConversations = ({ conversations }) => {
   return async (dispatch, getState) => {
     dispatch(slice.actions.fetchDirectConversations({ conversations }));
+  };
+};
+export const AddDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.addDirectConversation({ conversation }));
+  };
+};
+export const UpdateDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.updateDirectConversation({ conversation }));
   };
 };
